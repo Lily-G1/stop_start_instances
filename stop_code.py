3e445c224a32712488f31b182bd42267e1faa204
Lilian Galadima <anyagaladima@gmail.com>
2023-12-04 23:16:52 +0100

    modified code files

--- stop_code.py
+++ stop_code.py
@@ -1,6 +1,6 @@
 import boto3
 region = 'us-east-1'
-instances = ['i-0de95b80016c4baa1']
+instances = ['i-02931aa6df0a2b1c7']
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
